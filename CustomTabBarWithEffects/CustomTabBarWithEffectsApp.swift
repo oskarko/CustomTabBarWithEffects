c4a285e48cb1ae98f80e5de2de20947c038d957e
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2021-08-27 12:16:50 +0200

    Initial commit

---
+++ CustomTabBarWithEffects/CustomTabBarWithEffectsApp.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTabBarWithEffectsApp.swift
+//  CustomTabBarWithEffects
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2021 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarWithEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
